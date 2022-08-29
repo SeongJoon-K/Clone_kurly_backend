1be6058555188c4fe4edba095b8bb44489f2b66e
SEONGJOON KIM <jobcho6320@naver.com>
2022-08-29 16:43:56 +0900

    MODIFY DB

--- api.js
+++ api.js
@@ -5,6 +5,8 @@ const server = app.listen(3001, () => {
     console.log('START sever : localhost:3001')
 });
 
+
+
 app.get('/api/users/:type', async (req, res) => {
     let {
         type
@@ -13,5 +15,23 @@ app.get('/api/users/:type', async (req, res) => {
     console.log(type);
     res.send('ok');
 })
+
+
+app.post('/products', async (req, res) => {
+	const { category_id ,title, thumbnail, description, price } = req.body
+    
+	await myDataSource.query(
+		`INSERT INTO products(
+		    category_id,
+		    title,
+		    thumbnail,
+            description,
+            price
+		) VALUES (?, ?, ?, ?, ?);
+		`,
+		[ category_id ,title, thumbnail, description, price]
+	); 
+     res.status(201).json({ message : "successfully created" });
+	})
 
 // sample.
