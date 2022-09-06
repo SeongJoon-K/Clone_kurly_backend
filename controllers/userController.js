ed440ddc7d84a1b278347b73a0a8710da921b249
SEONGJOON KIM <jobcho6320@naver.com>
2022-09-06 11:11:19 +0900

    [ADD] 로그인bcrypt 설정

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const userService = require('../services/userService');
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = "JwTsEcReTkEyOrHaShInG";
+const bcrypt = require('bcrypt');
+
 
 const signUp = async (req, res) => {
   try {
@@ -20,12 +22,35 @@ const signUp = async (req, res) => {
   }
 };
 
+const saltRounds = 12;
 const login = async (req, res, next) => {
   try {
     const { login_id, password } = req.body;
+    // Bcrypt 암호화
+    console.log("userController 암호화 시작")
+    const makeHash = async (password, saltRounds) => {
+      return await bcrypt.hash(password, saltRounds);
+    }
+    console.log("userController bcrypt ok")
+    const main = async () => {
+      const hashedPassword = await makeHash(password, saltRounds);
+      console.log(hashedPassword);
+    }
+    // Bcrypt 검증
+    // const checkHash = async (password, hashedPassword) => {
+    //   return await bcrypt.compare(password, hashedPassword);
+    // }
+    // const main = async () => {
+    //   const hashedPassword = await makeHash(password, 12);
+    //   const result = await checkHash(password, hashedPassword);
+    //   console.log(result);
+    // }
+    main()
+    // id or pw 없거나, 불일치시 에러 출력함
     if( !login_id || !password ) {
       return res.status(400).json({ message : "Idpw error"});
     }
+
     const accessToken = await userService.login(login_id, password);
     if (accessToken === null) {
       return res.status(400)
