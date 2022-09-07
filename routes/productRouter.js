d96cfd6b636d4909b05993276b7594b855c4881e
SeongJoon-K <jobcho6320@naver.com>
2022-09-07 11:05:48 +0900

    [MODIFY] 상품(상세) GET 엔드포인트

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,9 +3,9 @@ const productController = require('../controllers/productController');
 
 const router = express.Router();
 
-router.post('/producted', productController.products);
-router.get('/', productController.getproduct);
-router.get('/:id', productController.detailproduct);
+router.post('/', productController.products); // 상품 데이터 생성
+router.get('/', productController.getproduct); // 상품 목록에 있는 상품 리스트 출력
+router.get('/:id', productController.detailproduct); // 상품 상세 정보 출력
 
 module.exports = {
     router
