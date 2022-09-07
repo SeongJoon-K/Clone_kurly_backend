7444063f61d1885f6b3abc1748737fe7c0cd40b1
SeongJoon-K <jobcho6320@naver.com>
2022-09-07 16:20:18 +0900

    [MODIFY] bcrypt & JWT

--- models/userDao.js
+++ models/userDao.js
@@ -20,25 +20,20 @@ myDataSource.initialize()
 
 // userDao 라우팅이 안되어 있어 app.js 에서 POST req 사용하였음.
 
-const createUser = async ( login_id, password, name ) => {
-	try {
-		return await myDataSource.query(
-		`INSERT INTO users(
-		    login_id,
-		    password,
-		    name
-		) VALUES (?, ?, ?);
-		`,
-		[ login_id, password, name ]
-	  );
-	} catch (err) {
-		const error = new Error('INVALID_DATA_INPUT');
-		error.statusCode = 500;
-		throw error;
-	}
+const createUser = async ( login_id, hashPw, name ) => {
+	console.log("hash PW : ",hashPw)
+	return await myDataSource.query(
+	`INSERT INTO users(
+		login_id,
+		password,
+		name
+	) VALUES (?, ?, ?);
+	`,
+	[ login_id, hashPw, name ]
+	);
 };
 
-const login = async ( login_id ) => {
+const login = async ( login_id, password) => {
 	const data = await myDataSource.query(
 		`SELECT * FROM users
 		WHERE login_id=?`,
--- services/userService.js
+++ services/userService.js
@@ -1,51 +1,55 @@
 const userDao = require('../models/userDao')
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const { useContainer } = require('typeorm');
 
 const signUp = async (login_id, password, name) => {
     if (!password.includes('@')){
       const err = new Error('@ 포함되지 않았습니다.');
-      err.statusCode = 409;
+      err.statusCode = 400;
       throw err;    
     }
     if (password.length < 10){
       const err = new Error('PW가 짧습니다.');
-      err.statusCode = 409;
+      err.statusCode = 400;
       throw err;        
     }
+  
+    const hashPw = await bcrypt.hash(password, 8); // Bcrypt 암호화
       const createUser = await userDao.createUser(
         login_id,
-        password,
+        hashPw, 
         name
         );
         return createUser;
-      };
+};
 
 
 
 // 로그인 POST API JWT 인증
 const login = async (login_id, password) => {
+  // const hashPw = await bcrypt.hash(password, 8);
   const user = await userDao.login(login_id, password);
-  if (user.password === password) {
-    const accessToken = jwt.sign(
-      user,
-      process.env.SECRET_KEY, 
-      {expiresIn: '1d'},
-  );
-  console.log(accessToken);
+  if (bcrypt.compare(password, user.password)) {
+    const accessToken = jwt.sign(login_id, process.env.SECRET_KEY);
+    console.log(accessToken);
     return accessToken;
-  } else {
-    return null;
-  }
+  } 
 }
+  // if (user.password === password) {
+  //   const accessToken = jwt.sign(
+  //     user,
+  //     process.env.SECRET_KEY, 
+  //     {expiresIn: '1d'},
+  // );
+
 
 // const check = async(id) => {
 //   const check = await userDao.check(id);
 //   return check;
 // }
   
-  module.exports = {
-      signUp,
-      login,
-
-  }
+module.exports = {
+    signUp,
+    login
+}
