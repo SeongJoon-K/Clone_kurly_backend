140d9cf00ad4b6998c923af2c763810164f0f1fb
SeongJoon-K <jobcho6320@naver.com>
2022-09-08 15:18:44 +0900

    [MODIFY] 상품상세 GET

--- models/productDao.js
+++ models/productDao.js
@@ -52,7 +52,7 @@ const getproduct = async () => {
 const detailproduct = async(id) => {
     console.log(id);
     const product = await myDataSource.query(
-        `SELECT title, description, price, discount FROM products WHERE id=?;`,(id))
+        `SELECT * FROM products WHERE id=?;`,(id))
     return product[0]; // RowDataPocket이 []로 감싸져 있어서 [0] 추가하였음
 }
 
