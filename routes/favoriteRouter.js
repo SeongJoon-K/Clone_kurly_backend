dfcd7555bb0f3697fa3de46572382cd339ced35b
SEONGJOON KIM <jobcho6320@naver.com>
2022-09-11 18:57:04 +0900

    [ADD] 찜목록 router

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -3,4 +3,7 @@ const favoriteController = require('../controllers/favoriteController');
 
 const router = express.Router();
 
+
+router.get('/', favoriteController.getfavorite); // 해당 유저의 찜목록 HTTP
+
 // 찜목록 API
