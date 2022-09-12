cd55235fc708ecdffba92aadc0fdeee3c115d452
SEONGJOON KIM <jobcho6320@naver.com>
2022-09-12 23:57:03 +0900

    [ADD] 찜목록 GET

--- services/favoriteService.js
+++ services/favoriteService.js
@@ -1,2 +1,10 @@
 const favoriteDao = require('../models/favoriteDao');
 
+const getfavorite = async() => {
+    const favorite = await favoriteDao.getfavorite(product_id);
+    return favorite;
+}
+
+module.exports = {
+    getfavorite
+}
