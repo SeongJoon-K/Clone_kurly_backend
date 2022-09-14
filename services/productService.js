0e0edc5840d55101b49f831dac39463eecc5c632
SeongJoon-K <jobcho6320@naver.com>
2022-09-14 16:14:43 +0900

    [MODIFY] Token input type

--- services/productService.js
+++ services/productService.js
@@ -1,38 +1,44 @@
-const productDao = require('../models/productDao');
+const productDao = require("../models/productDao");
 
-const products = async (category_id ,title, thumbnail, description, price, discount) => {
-    const maxPrice = 200000000;
-    if (price > maxPrice) {
-        const err = new Error('상품 금액을 잘 못 설정하였습니다.');
-        err.statusCode = 400;
-        throw err;
-    }   
-    const createProduct = await productDao.createProduct(
-        category_id,
-        title,
-        thumbnail,
-        description,
-        price,
-        discount
-    );
-    return createProduct;
+const products = async (
+  category_id,
+  title,
+  thumbnail,
+  description,
+  price,
+  discount
+) => {
+  const maxPrice = 200000000;
+  if (price > maxPrice) {
+    const err = new Error("상품 금액을 잘 못 설정하였습니다.");
+    err.statusCode = 400;
+    throw err;
+  }
+  const createProduct = await productDao.createProduct(
+    category_id,
+    title,
+    thumbnail,
+    description,
+    price,
+    discount
+  );
+  return createProduct;
 };
 
 // 상품목록 GET service
-const getproduct = async() => {
-    const product = await productDao.getproduct();
-    return product;
-
-}   
+const getproduct = async () => {
+  const product = await productDao.getproduct();
+  return product;
+};
 
 // 상품상세 GET service
-const detailproduct = async(id) => {
-    const product = await productDao.detailproduct(id);
-    return product;
-}
+const detailproduct = async (id) => {
+  const product = await productDao.detailproduct(id);
+  return product;
+};
 
 module.exports = {
-    products,
-    getproduct,
-    detailproduct
-}
+  products,
+  getproduct,
+  detailproduct,
+};
