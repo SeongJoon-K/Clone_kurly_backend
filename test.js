f1ac4af999d3d07de547a43a0f3c9c98ff1a7844
SeongJoon-K <jobcho6320@naver.com>
2022-09-20 15:59:50 +0900

    [MODIFY] 상품 Query builder

--- test.js
+++ test.js
@@ -1,45 +1,30 @@
-const categoryId = 1;
-const minPrice = 123;
-// const maxPrice = undefined
-// WHERE가 들어가는 조건이 들어갔을 때,
-function productCondition(...params) {
-  // 배열 형태로 들어감
-  const productConditionArray = [];
+// 기존 Query Builder code
+function productCondition(categoryId, minPrice, maxPrice) {
+  let conditionStr = [" WHERE"];
+  const categoryIdString = ` categoryId = ${categoryId}`;
+  const minPriceString = ` price >= ${minPrice}`;
+  const maxPriceString = ` price <= ${maxPrice}`;
   if (categoryId) {
-    const categoryIdString = ` categoryId=${categoryId}`;
-    productConditionArray.push(categoryId);
+    conditionStr.push(categoryIdString);
+  } else if (categoryId && conditionStr.length !== 1) {
+    conditionStr.push(` AND`);
+    conditionStr.push(categoryIdString);
   }
-  if (minPrice) {
-    const minPriceString = ` price >= ${minPrice}`;
-    productConditionArray.push(minPrice);
+  if (minPrice && conditionStr.length === 1) {
+    conditionStr.push(minPriceString);
+  } else if (minPrice && conditionStr.length !== 1) {
+    conditionStr.push(` AND`);
+    conditionStr.push(minPriceString);
   }
-  if (maxPrice) {
-    const maxPriceString = ` price <= ${maxPrice}`;
-    productConditionArray.push(maxPrice);
+  if (maxPrice && conditionStr.length === 1) {
+    conditionStr.push(maxPriceString);
+  } else if (maxPrice && conditionStr.length !== 1) {
+    conditionStr.push(` AND`);
+    conditionStr.push(maxPriceString);
   }
-
-  console.log(params, "check");
-  // 해당 조건의 값이 정의 되었을 때 push 함
-  console.log(productConditionArray);
-  // 해당 값이 존재할 때만 배열에 넣어야함.
+  const whereStr = conditionStr.join("");
+  return whereStr;
 }
-productCondition(categoryId, minPrice);
-/* 
-필요한 조건에 대해서 
-
-  1. 해당 조건이 없는 경우
-  조건이 없을 시 함수 미사용함 고려 X
-
-  2. 해당 조건이 하나인 경우 params == 1
-  해당 조건이 하나인 경우 WHERE만 쓴다
-
-  3. 해당 조건이 여러개인 경우 params >=  2
-  WHERE과 AND 가 있어야함 
-
-
+/* 2, 4, 6 
+W a A a  
 */
-
-//
-function productOrderBy() {}
-
-function productLimit() {}
