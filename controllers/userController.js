e6962da8ea08f44ad9d5df88c30033229d0a41e5
SEONGJOON KIM <jobcho6320@naver.com>
2022-12-05 10:34:24 +0900

    feat: add userController dotenv

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-const userService = require("../services/userService");
+const userService = require('../services/userService');
 
 const signUp = async (req, res) => {
   try {
     const { loginId, password, name } = req.body;
     if (!loginId || !password || !name) {
-      return res.status(400).json({ message: "KEY_ERROR" });
+      return res.status(400).json({ message: 'KEY_ERROR' });
     }
     const signupPost = await userService.signUp(loginId, password, name);
-    res.status(201).json({ message: "Created Successful " });
+    res.status(201).json({ message: 'Created Successful ' });
   } catch (err) {
     console.log(err);
     return res.status(err.statusCode || 500).json({ message: err.message });
@@ -23,7 +23,7 @@ const startKakao = (req, res) => {
   const config = {
     client_id: process.env.KAKAO_CLIENT_ID,
     redirect_uri: process.env.KAKAO_REDIRECT_URI,
-    response_type: "code",
+    response_type: 'code',
   };
   const params = new URLSearchParams(config).toString();
 
@@ -33,20 +33,20 @@ const startKakao = (req, res) => {
 };
 
 const finishKakao = async (req, res) => {
-  const baseUrl = "https://kauth.kakao.com/oauth/token";
+  const baseUrl = 'https://kauth.kakao.com/oauth/token';
   const config = {
-    client_id: "4e4e9b6459b97ee7e4666f357bbfb85c",
-    client_secret: "rbxHtMaLFfb0NJS5KCP78O6IEBDQrVG7",
-    grant_type: "authorization_code",
-    redirect_uri: "http://127.0.0.1:3000/kakao/finish",
+    client_id: '4e4e9b6459b97ee7e4666f357bbfb85c',
+    client_secret: 'rbxHtMaLFfb0NJS5KCP78O6IEBDQrVG7',
+    grant_type: 'authorization_code',
+    redirect_uri: 'http://127.0.0.1:3000/kakao/finish',
     code: req.query.code,
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
   const kakaoTokenRequest = await fetch(finalUrl, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-type": "application/json", // 이 부분을 명시하지않으면 text로 응답을 받게됨
+      'Content-type': 'application/json', // 이 부분을 명시하지않으면 text로 응답을 받게됨
     },
   });
   const json = await kakaoTokenRequest.json();
@@ -57,11 +57,11 @@ const finishKakao = async (req, res) => {
 const login = async (req, res) => {
   const { loginId, password } = req.body;
   if (!loginId || !password) {
-    return res.status(400).json({ message: "ID or PW IS UNDEFINED" });
+    return res.status(400).json({ message: 'ID or PW IS UNDEFINED' });
   }
   const accessToken = await userService.login(loginId, password);
   if (!accessToken) {
-    return res.status(400).json({ message: "JWT IS UNDEFINED" });
+    return res.status(400).json({ message: 'JWT IS UNDEFINED' });
   }
   res.status(200).json(accessToken);
 };
